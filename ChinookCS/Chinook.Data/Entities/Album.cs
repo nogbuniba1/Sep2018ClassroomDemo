6d263d9ffc5cda9c1f1b18fb19c8bf3d94e48f02
nogbuniba1 <39070816+nogbuniba1@users.noreply.github.com>
2018-09-10 20:39:49 -0600

    Create Chinook Solution, Data and System

---
+++ ChinookCS/Chinook.Data/Entities/Album.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespace
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+
+#endregion
+
+
+namespace Chinook.Data.Entities
+{
+    [Table("Albums")]
+    public class Album
+    {
+        [Key]
+        public int AlbumId { get; set; }
+
+        [Required(ErrorMessage ="Title is required")]
+        [StringLength(160, ErrorMessage = "Title is limited to 160 characters")]
+        public string Title { get; set; }
+
+        public int ArtistId { get; set; }       //The default value of an "int" is 0, so we don't need to put the required error message
+
+        public int ReleaseYear { get; set; }
+
+        [StringLength(50, ErrorMessage = "Release label is limited to 50 characters")]
+        public string ReleaseLabel { get; set; }
+    }
+}
---
+++ ChinookCS/Chinook.Data/Entities/Artist.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+#endregion
+
+namespace Chinook.Data.Entities
+{
+    [Table("Artists")]
+    public class Artist
+    {
+        //This is the default for Primary key - [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        //For a compound Key - [Key, Column (Order = 1)] for the first key, [Key, Column (Order = 2)] for the 2nd key etc.
+
+        [Key]                  
+        public int ArtistId { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(120, ErrorMessage = "Name is limited to 120 Characters")]
+        public string Name { get; set; }
+    }
+}
